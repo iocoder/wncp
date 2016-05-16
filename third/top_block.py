2ed6c772b76440228c897e51cdef633a1acbff6c
iocoder <iocoder@aol.com>
2016-05-16 17:03:25 +0200

    modified README file

--- third/top_block.py
+++ third/top_block.py
@@ -3,10 +3,11 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Top Block
-# Generated: Thu Apr 14 17:36:51 2016
+# Generated: Fri Apr 22 10:24:32 2016
 ##################################################
 
 from gnuradio import blocks
+from gnuradio import digital
 from gnuradio import eng_notation
 from gnuradio import gr
 from gnuradio import uhd
@@ -38,14 +39,26 @@ class top_block(gr.top_block):
         )
         self.uhd_usrp_source_0.set_samp_rate(samp_rate)
         self.uhd_usrp_source_0.set_center_freq(1.241e9, 0)
-        self.uhd_usrp_source_0.set_gain(0, 0)
-        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_gr_complex*1, "output", False)
+        self.uhd_usrp_source_0.set_gain(50, 0)
+        self.digital_psk_demod_0 = digital.psk.psk_demod(
+          constellation_points=2,
+          differential=True,
+          samples_per_symbol=1,
+          excess_bw=0.35,
+          phase_bw=6.28/100.0,
+          timing_bw=6.28/100.0,
+          mod_code="gray",
+          verbose=False,
+          log=False,
+          )
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_char*1, "output", False)
         self.blocks_file_sink_0.set_unbuffered(False)
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.uhd_usrp_source_0, 0), (self.blocks_file_sink_0, 0))    
+        self.connect((self.digital_psk_demod_0, 0), (self.blocks_file_sink_0, 0))    
+        self.connect((self.uhd_usrp_source_0, 0), (self.digital_psk_demod_0, 0))    
 
     def get_samp_rate(self):
         return self.samp_rate
